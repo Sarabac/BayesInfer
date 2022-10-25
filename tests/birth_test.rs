db1a473f5449ca2a809b00e59b487d9f0803c8fe
lucasb <lucas.boutar@gmail.com>
2022-10-25 16:41:16 +0200

    tentative test

---
+++ tests/birth_test.rs
@@ -0,0 +1,50 @@
+use rust_db::bayes_model::BayesModel;
+use serde::{Deserialize, Serialize};
+use sqlx::PgPool;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BinomTest {
+    p: f64,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Genre {
+    f: i8,
+}
+
+fn init_binom(bin: &BinomTest) -> f64 {
+    bin.p
+}
+
+fn model_binom(bin: &BinomTest, genre: &Genre) -> f64 {
+    bin.p * (genre.f as f64)
+}
+
+#[sqlx::test]
+async fn birth(conn: PgPool) -> sqlx::Result<()> {
+    
+    let iter_binom = (1..10).map(|n| BinomTest {
+        p: 1f64 / (n as f64),
+    });
+    
+    let iter_genre = vec![Genre { f: 0 }, Genre { f: 1 }];
+
+    
+    let model: BayesModel<BinomTest, Genre> = BayesModel::connect(conn).await;
+
+    model.add_hypo(iter_binom).await?;
+    model.add_record(iter_genre).await?;
+
+    let test_get_hypo = model.get_hypo().await;
+    assert!(test_get_hypo.len() == 9);
+    let nb_model = model.add_model_fn(model_binom).await;
+    assert!(nb_model.0 == 9 * 2 && nb_model.1 == 0);
+    let vec_model = model.get_model_all().await;
+    assert!(vec_model.len() == 9 * 2);
+
+    //let string_model = read_model(&client).await;
+    //println!("{}", string_model);
+    println!("init prior");
+    //init_prior(&client, |h: &BinomTest| h.p).await;
+
+    Ok(())
+}
